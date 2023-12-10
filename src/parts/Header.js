b98c69fd91a3fd8d545210de1d3b8c289bf4fa50
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2023-12-10 23:01:07 +0100

    Creation of Home and Projects page with the home title and the app header

---
+++ src/parts/Header.js
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+import '../assets/css/parts/Header.css';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <nav className="links">
+                <div>
+                    <Link to="/">Home</Link>
+                </div>
+                <div>
+                    <Link to="/projects">Projects</Link>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
