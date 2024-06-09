d76548b31a3f18c8d30b33c3f30d39524816b11f
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-09 03:23:17 -0700

    Display in a good way data for Certifications page.

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,17 +1,44 @@
-import Title from "../components/generic/Title";
 import {useEffect, useState} from "react";
+import parse from "html-react-parser";
+import Title from "../components/generic/Title";
+import '../styles/pages/Certifications.css';
 
 const Certifications = () => {
-    const [certifications, setCertifications] = useState([]);
+    const [certifications, setCertifications] = useState(null);
     useEffect(() => {
         fetch('/protected_assets/data/certifications.json')
             .then(response => response.json())
             .then(data => setCertifications(data))
     }, []);
-    return (
-        <div>
-            <Title title={certifications.title}
-                     subtitle={certifications.subtitle}/>
+
+    console.log(certifications);
+
+    return certifications && (
+        <div className="certifications">
+            <div>
+                <Title title={certifications.title}
+                       subtitle={certifications.subtitle}/>
+            </div>
+            <div className="certifications-list">
+                {(certifications.items).map((item) => {
+                    return (
+                        <div key={item.title} className="certification">
+                            <div className="image" data-aos="fade-right">
+                                <a href={item.link} target="_blank" rel="noreferrer">
+                                    <img src={"/protected_assets/images/" + item.image} alt={item.image}/>
+                                </a>
+                            </div>
+                            <div className="text" data-aos="fade-left">
+                                <a href={item.link} target="_blank" rel="noreferrer">
+                                    {item.title}
+                                </a>
+                                <div>{parse(item.description)}</div>
+                                <em>{item.date}</em><br/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
