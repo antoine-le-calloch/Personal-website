e0df20ed6d80abc5a546b38cb1392dd11cd567fc
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-13 00:01:29 -0700

    Improve footer.

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -5,20 +5,24 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="top-footer">
-                <div className="links">
+                <div className="sites">
+                    <span className="line"></span>
                     <a href="https://www.linkedin.com/in/antoine-le-calloch" target="_blank" rel="noreferrer">
                         <BsLinkedin/>
                     </a>
                     <a href="https://github.com/antoine-le-calloch" target="_blank" rel="noreferrer">
                         <BsGithub/>
                     </a>
+                    <span className="line"></span>
+                </div>
+                <div className="infos">
                     <a href="mailto:email">Email</a>
                 </div>
             </div>
 
             <div className="bottom-footer">
                 <div>
-                    &copy; 2021 - All rights reserved
+                    © 2024 - All rights reserved. Developed by Antoine Le Calloch.
                 </div>
             </div>
         </footer>
