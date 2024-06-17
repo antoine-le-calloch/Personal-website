ef096f116874753d6dc3a010e5cca7b88883f328
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-17 01:20:16 -0700

    Creation of a right panel menu for mobile version with open icon.

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -6,6 +6,7 @@ import '../styles/parts/Header.css';
 
 const Header = () => {
     const [checked, setChecked] = useState(false);
+    const [openMenu, setOpenMenu] = useState(false);
     const root = document.documentElement;
     const handleSwitch = (state) => {
         setChecked(state);
@@ -15,13 +16,18 @@ const Header = () => {
             root.classList.remove('dark');
         }
     };
+    const handleMenuMobile = () => {
+    };
     return (
         <header className="header">
             <nav>
                 <Link className="logo" to="/">
                         <img src="/logo/logo_ALC.png" alt="Logo ALC"/>
                 </Link>
-                <div className="menu">
+                <button className="open-menu-mobile" onClick={() => setOpenMenu(!openMenu)}>
+                    <span  className={openMenu ? "open" : ""}/>
+                </button>
+                <div className={"menu" + (openMenu ? " open" : "")}>
                     <Link to="/">Home</Link>
                     <Link to="/experiences-and-education">Experiences / Education</Link>
                     <Link to="/certifications">Certifications</Link>
