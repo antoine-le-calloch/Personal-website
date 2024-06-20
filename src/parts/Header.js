cfd485f33973ee86c504a5621effdacb8bd0e879
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-06-19 21:25:11 -0700

    Improve header menu panel to close when click on link or when click outside the panel.

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Link} from "react-router-dom";
 import Switch from "react-switch";
 import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
@@ -7,28 +7,50 @@ import '../styles/parts/Header.css';
 const Header = () => {
     const [checked, setChecked] = useState(false);
     const [openMenu, setOpenMenu] = useState(false);
-    const root = document.documentElement;
+    const menuRef = useRef(null);
+
     const handleSwitch = (state) => {
-        setChecked(state);
+        const root = document.documentElement;
         if (state) {
             root.classList.add('dark');
         } else {
             root.classList.remove('dark');
         }
+        setChecked(state);
     };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            console.log(menuRef)
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpenMenu(false);
+            }
+        };
+
+        if(openMenu && window.innerWidth < 1000){
+            document.addEventListener('mousedown', handleClickOutside);
+        }else{
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [openMenu, menuRef]);
+
     return (
         <header className="header">
             <nav>
                 <Link className="logo" to="/">
                         <img src="/logo/logo_ALC.png" alt="Logo ALC"/>
                 </Link>
-                <button className="open-menu-mobile" onClick={() => setOpenMenu(!openMenu)}>
+                <button ref={menuRef} className="open-menu-mobile" onClick={() => setOpenMenu(!openMenu)}>
                     <span  className={openMenu ? "open" : ""}/>
                 </button>
-                <div className={"menu" + (openMenu ? " open" : "")}>
-                    <Link to="/">Home</Link>
-                    <Link to="/experiences-and-education">Experiences / Education</Link>
-                    <Link to="/certifications">Certifications</Link>
+                <div ref={menuRef} className={"menu" + (openMenu ? " open" : "")}>
+                    <Link to="/" onClick={() => setOpenMenu(false)}>Home</Link>
+                    <Link to="/experiences-and-education"  onClick={() => setOpenMenu(false)}>Experiences / Education</Link>
+                    <Link to="/certifications" onClick={() => setOpenMenu(false)}>Certifications</Link>
                     {/* use css var primary color */}
                     <Switch className="switch" checked={checked} onChange={state => handleSwitch(state)}
                             uncheckedIcon={<BsFillSunFill />}
