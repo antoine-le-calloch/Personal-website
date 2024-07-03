a204f09882ce63cd23c355afc97bdaa9db5ba56e
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-07-03 01:16:18 -0700

    Add languages, frameworks and development skills to the home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,57 @@ const Home = () => {
     }, []);
     return introduction && (
         <div className="home">
-            <h1 data-aos="fade-in">{introduction.title}</h1>
-            <Text data-aos="fade-in" text={introduction.text}/>
+            <div className="title">
+                <h1 data-aos="fade-in">{introduction.title}</h1>
+                <Text data-aos="fade-in" text={introduction.text}/>
+            </div>
+            <div className="skills">
+                <div className="skill languages">
+                    <h2>Computer languages</h2>
+                    <div className="schema">
+                        {introduction.skills.languages.map((language, index) => (
+                            <div key={language.name} className="language" 
+                                 style={{
+                                     height: `${language.level}%`,
+                                     background: introduction.colors[index],
+                                     animationDelay: `${index * 0.5}s`
+                                 }}>
+                                {language.name}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="skill frameworks">
+                    <h2>Frameworks</h2>
+                    <div className="schema">
+                        {introduction.skills.frameworks.map((framework, index) => (
+                            <div key={framework.name} className="framework"
+                                 style={{
+                                     height: `${framework.level}%`,
+                                     background: introduction.colors[index],
+                                     animationDelay: `${index * 0.5}s`
+                                 }}>
+                                {framework.name}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="skill development-tools">
+                    <h2>Development tools</h2>
+                    <div className="schema">
+                        {introduction.skills.development_tools.map((tool, index) => (
+                            <div key={tool.name} className="tool"
+                                 style={{
+                                     height: `${tool.level}%`,
+                                     background: introduction.colors[index],
+                                     animationDelay: `${index * 0.5}s`
+                                 }}>
+                                {tool.name}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
